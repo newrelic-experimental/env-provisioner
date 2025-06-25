7738c6956d56fc9b03f5729b0028843aeefb4e56
Abhishu Raina <araina@newrelic.com>
2025-06-25 14:09:39 +0530

    remove alma-8 ami

--- terraform/otel-ec2/vars.tf
+++ terraform/otel-ec2/vars.tf
@@ -431,19 +431,6 @@ variable "ec2_otels" {
           "otel_role" = "agent"
         }
       }
-      "amd64:alma-8" = {
-        ami             = "ami-0f4ab906702bed7d5"
-        subnet          = "subnet-0c2046d7a0595aa2c"
-        security_groups = ["sg-075f379cc5612e984"]
-        key_name        = "caos-dev-arm"
-        instance_type   = "t4g.small"
-        username        = "ec2-user"
-        python          = "/usr/bin/python3"
-        platform        = "linux"
-        tags            = {
-          "otel_role" = "agent"
-        }
-      }
       "arm64:alma-8" = {
         ami             = "ami-01d5ed1fa2aab982a"
         subnet          = "subnet-0c2046d7a0595aa2c"
