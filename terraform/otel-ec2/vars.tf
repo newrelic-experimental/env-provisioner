ad2689a44d91d72c78deef9d19977fff20c7d21d
Abhishu Raina <araina@newrelic.com>
2025-06-26 21:26:45 +0530

    update instance types

--- terraform/otel-ec2/vars.tf
+++ terraform/otel-ec2/vars.tf
@@ -431,12 +431,12 @@ variable "ec2_otels" {
           "otel_role" = "agent"
         }
       }
-      "x86_64:alma-8" = {
+      "amd64:alma-8" = {
         ami             = "ami-02825266d2b3af325"
         subnet          = "subnet-0c2046d7a0595aa2c"
         security_groups = ["sg-075f379cc5612e984"]
         key_name        = "caos-dev-arm"
-        instance_type   = "t4g.small"
+        instance_type   = "t3a.small"
         username        = "ec2-user"
         python          = "/usr/bin/python3"
         platform        = "linux"
@@ -457,12 +457,12 @@ variable "ec2_otels" {
           "otel_role" = "agent"
         }
       }
-      "x86_64:alma-9" = {
+      "amd64:alma-9" = {
         ami             = "ami-004ab7ad00a70a340"
         subnet          = "subnet-0c2046d7a0595aa2c"
         security_groups = ["sg-075f379cc5612e984"]
         key_name        = "caos-dev-arm"
-        instance_type   = "t4g.small"
+        instance_type   = "t3a.small"
         username        = "ec2-user"
         python          = "/usr/bin/python3"
         platform        = "linux"
